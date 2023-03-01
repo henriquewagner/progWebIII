9956afe4d93af03b61796511e3ff0ea4a166ab16
Henrique Bernard Wagner <henriquebernardwagner@gmail.com>
2023-02-28 22:32:37 -0300

    init commit: server.ts 2

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,8 @@ const app = express()
 const port = 3000
 
 app.get("/", (request, response ) => {
-    response.send('Hello World - FEMA')
+    //response.send('Hello World - FEMA')
+    response.json({msg:"Fim da Aula"});
 });
 
 app.listen(port, () => {
